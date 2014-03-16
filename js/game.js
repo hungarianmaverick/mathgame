af4ce2c7a680e9f8f5b69e570065d3a061396574
unknown <tamas.boros@outlook.com>
2014-03-16 23:23:15 +0000

    First working proto, added stopwatch and misses for each answers

--- js/game.js
+++ js/game.js
@@ -1,79 +1,75 @@
 function MathGame() {
+
+    var self=this;
+
     this.setStatus =  function (status){
-        $('#'+this.statusField).text(status);
-        $('#'+this.statusField).fadeIn(200, function() {
-            $('#'+this.statusField).fadeOut(400);
+        $('#'+self.statusField).text(status);
+        $('#'+self.statusField).fadeIn(200, function() {
+            $('#'+self.statusField).fadeOut(400);
         })
     };
 
     this.clearInput = function (){
-        $('#'+this.inputField).val("");
+        $('#'+self.inputField).val("");
     };
 
     this.setQuestion = function (a, b){
-        $('#'+this.questionField).text(a.toString() + getSignedNumber(b) + "=");
+        $('#'+self.questionField).text(a.toString() + getSignedNumber(b) + "=");
     };
 
     this.nextRound = function(){
-        res = this.scramble();
-        before = new Date().getTime();
-        $('#txt').keydown(function (e){
+        $('#'+self.inputField).keydown(function (e){
             if(e.keyCode == 13){
-                var time = new Date().getTime() - before
-                if ($('#txt').val() == res){
-                    setStatus("Good!");
-                    res = scramble();
-                    before = new Date().getTime();
-                }
-                else{
-                    setStatus("Wrong!!");
-                }
+                answer = $('#'+self.inputField).val();
+                self.checkInput(e, answer);
             }
         })	
     };
 
+    this.checkInput = function(e, answer){
+        var time = new Date().getTime() - self.lastScrambleTime;
+        if (answer == self.currentSolution){
+            self.timeResults[self.questionId] = time;
+            self.missesResults[self.questionId] = self.currentMisses;
+            self.setStatus("Good!");
+            self.scramble();
+        }
+        else{
+            self.currentMisses++;
+            self.setStatus("Wrong!!");
+        }
+    }
+
     this.scramble = function(){
         var a = getRandomInt(-99, 99);
         var b = a+getRandomInt(-99, 99);
-        var res = a+b;
+        var solution = a+b;
 
+        this.currentSolution = solution;
+        this.currentMisses = 0;
         this.setQuestion(a, b);
         this.clearInput();
-
-        return res;
-    };
-
-    this.mainTimer = function()
-    {
-        if (this.timeAvailable-->0){
-            this.gauge.refresh(this.timeAvailable);
-        }
-        else{
-            stopMainTimer();
-            endGame();
-        }			
-    };
-
-    this.stopMainTimer = function()
-    {
-        clearInterval(this.mainTimerHandler);
-    };
-
-    this.endGame = function()
-    {
-        alert("Game over!");
+        this.lastScrambleTime = new Date().getTime();
+        this.questionId++;
     };
 
     this.startGame = function() {
-        
+        this.startTime = new Date().getTime();
+        this.questionId = 0;
+        this.scramble();
         this.nextRound();
     }
+
+    this.endGame = function(){
+        alert("Game over!");
+    };
 
     this.timeAvailable = 60;
     this.questionField = 'question';
     this.inputField = 'input';
     this.statusField = 'status';
-    
+    this.timeResults = {};
+    this.missesResults = {};
 
     this.gauge = new JustGage({
         id: "gauge", 
@@ -81,10 +77,27 @@ function MathGame() {
         min: 0,
         max: this.timeAvailable,
         title: "Time left",
-        levelColors: ['#E81700', '#FC95FF', '#E1E859', '#CAFFD1']
+        levelColors: ['#E81700', '#FF530D', '#FFED60', '#CAFFD1']
     });
 
-    this.mainTimerHandler = setInterval(this.mainTimer(),1000);
+    this.stopMainTimer = function(){
+        clearInterval(self.mainTimerHandler);
+    };
+
+    this.mainTimer = function(){
+        if (self.timeAvailable-->0){
+            self.gauge.refresh(self.timeAvailable);
+        }
+        else{
+            self.stopMainTimer();
+            self.clearInput();
+            self.endGame();
+        }           
+    };
+
+    this.mainTimerHandler = setInterval(this.mainTimer, 1000);
 
     this.startGame();
 };
+
+new MathGame();
