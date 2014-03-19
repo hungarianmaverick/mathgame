6a9161f54466ee8e7b6db726fcdee79a98067fdd
unknown <tamas.boros@outlook.com>
2014-03-19 22:22:52 +0000

    Major refactoring, code styling

--- js/game.js
+++ js/game.js
@@ -1,159 +1,14 @@
 function MathGame() {
 
+    // Required for callbacks, where "this"
+    // represents the window object
     var self=this;
 
-    this.setStatus =  function (status){
-        if (status){
-            $('#'+self.statusField).removeClass().addClass('glyphicon glyphicon-ok');
-        }
-        else{
-            $('#'+self.statusField).removeClass().addClass('glyphicon glyphicon-remove');
-        }
-
-        $('#'+self.statusField).animate({opacity: 1}, 200, function(){
-            $('#'+self.statusField).animate({opacity: 0}, 400);
-        });
-    };
-
-    this.displayElement = function (alpha, element){
-        $('#'+element).animate({opacity: alpha}, 200);
-    };
-
-    this.clearInput = function (){
-        $('#'+self.inputField).val("");
-    };
-
-    this.setQuestion = function (a, b){
-        $('#'+self.questionField).text(a.toString() + getSignedNumber(b) + "=");
-    };
-
-    this.checkInput = function(e, answer){
-        if (answer == self.currentSolution){
-            self.setStatus(true);
-            self.shuffle();
-            if (self.mode == "survival"){
-                self.addSeconds(3);
-            }
-        }
-        else{
-            self.missed++;
-            self.setStatus(false);
-            if (self.mode == "survival"){
-                self.addSeconds(-1);
-            }
-        }
-    };
-
-    this.addSeconds = function(seconds){
-        var futureValue = self.timeAvailable + seconds;
-
-        if (futureValue>60){
-            futureValue=60;
-        }
-
-        if (futureValue<0){
-            futureValue=0;
-        }
-
-        self.timeAvailable = futureValue;
-        self.gauge.refresh(self.timeAvailable);
-        // Do some animation stuff here +3 in green, -1 in red
-    };
-
-    this.shuffle = function(){
-        var a = getRandomInt(-99, 99);
-        var b = a+getRandomInt(-99, 99);
-        var solution = a+b;
-
-        this.currentSolution = solution;
-        this.setQuestion(a, b);
-        this.clearInput();
-
-        this.questionId++;
-        this.questions[this.questionId] = a.toString() + getSignedNumber(b);
-        this.missesResults[self.questionId] = 0;
-    };
-
-    this.startGame = function(mode) {
-        this.startTime = new Date().getTime();
-        this.started = true;
-
-        this.mainTimerHandler = setInterval(this.mainTimer, 1000);
-        this.gauge.refresh(self.timeAvailable);
-        this.mode = mode;
-        this.shuffle();
-
-        // Hide welcome screen
-        this.displayElement(0, "frmWelcome");
-        $('#frmWelcome').hide();
-
-        if (mode == "classic"){
-            this.timeAvailable = 60;
-            $('#lblMode').html('Classic mode');
-        }
-        else{
-            this.timeAvailable = 30;
-            $('#lblMode').html('Survival mode');
-        }
-        this.gauge.refresh(self.timeAvailable);
-
-        this.displayElement(1, this.gameField);
-
-        $('#'+this.inputField).focus()
-    };
-
-    this.endGame = function() {
-        this.started = false;
-
-        $('#lblScored').html(self.questionId-1);
-        $('#lblMissed').html(self.missed);
-        $('#lblTime').html(self.totalsecs);
-
-        // Hide game
-        this.displayElement(0, this.gameField);
-
-        // Show results screen
-        if (this.mode == "survival"){
-            $('#frmGameTime').show();
-        }
-        else{
-            $('#frmGameTime').hide();
-        }
-
-        $('#frmResults').show();
-        this.displayElement(1, "frmResults");
-    };
-
-    this.resetGame = function() {
-        this.timeResults = {};
-        this.missesResults = {};
-        this.questions = {};
-        this.missed = 0;
-        this.questionId = 0;
-        this.totalsecs = 0;
-    };
-
-    this.newGame = function() {
-        this.resetGame();
-
-        this.displayElement(0, "frmResults");
-        $('#frmResults').hide();
-
-        this.displayElement(0, "frmHelp");
-        $('#frmHelp').hide();
-
-        $('#frmWelcome').show();
-        this.displayElement(1, "frmWelcome");
-    };
-
     this.init = function(){
-        this.questionField = 'question';
-        this.inputField = 'input';
-        this.statusField = 'status';
-        this.gameField = 'game';
-
+        // Reset values
         this.resetGame();
 
+        // Initialize timer gauge
         this.gauge = new JustGage({
             id: "gauge", 
             value: 60, 
@@ -163,39 +18,177 @@ function MathGame() {
             levelColors: ['#E81700', '#FF530D', '#FFED60', '#CAFFD1']
         });
 
-        $('#'+this.inputField).keydown(function (e){
+        // Set up listener to user input
+        $('#userInput').keydown(function (e){
             if (self.started && e.keyCode == 13){
-                answer = $('#'+self.inputField).val();
+                answer = $('#userInput').val();
                 self.checkInput(e, answer);
             }
         })
 
-        this.displayElement(1, "frmWelcome");
+        // Show welcome screen
+        fadeElement(1, "#frmWelcome");
     };
 
-    this.stopMainTimer = function(){
-        clearInterval(self.mainTimerHandler);
+    this.resetGame = function() {
+        // Reset values
+        this.missed = 0;
+        this.questionId = 0;
+        this.totalsecs = 0;
     };
 
-    this.mainTimer = function(){
-        if (self.timeAvailable-->0){
+    this.startGame = function(mode) {
+        // Reset values
+        this.resetGame();
+
+        // Start game
+        this.started = true;
+        this.mainTimerHandler = setInterval(this.mainTimerTick, 1000);
+
+        // Set game mode
+        this.mode = mode;
+        
+        // Hide welcome screen
+        hideElement("#frmWelcome");
+        
+        // Show game field
+        fadeElement(1, "#frmGame");
+
+        // Set time
+        if (mode == "classic"){
+            this.timeAvailable = 60;
+            $('#lblMode').html('Classic mode');
+        }
+        else{
+            this.timeAvailable = 30;
+            $('#lblMode').html('Survival mode');
+        }
+
+        // Update timer
+        this.gauge.refresh(self.timeAvailable);
+
+        // Shuffle
+        this.shuffle();
+
+        // Focus
+        $('#userInput').focus();
+    };
+
+    this.endGame = function() {
+        // Stop game
+        this.started = false;
+
+        // Set results
+        $('#lblScored').html(self.questionId-1);
+        $('#lblMissed').html(self.missed);
+        $('#lblTime').html(self.totalsecs);
+
+        // Hide game
+        fadeElement(0, "#frmGame")
+
+        // No need to show game time
+        // in classic mode
+        if (this.mode == "survival"){
+            $('#frmGameTime').show();
+        }
+        else{
+            $('#frmGameTime').hide();
+        }
+
+        // Show results screen
+        showElement('#frmResults');
+    };
+
+    this.shuffle = function(){
+        // Generate a random operation
+        var a = getRandomInt(-99, 99);
+        var b = a+getRandomInt(-99, 99);
+        var solution = a+b;
+
+        // Update UI
+        this.currentSolution = solution;
+        setQuestion(a, b, "#lblQuestion");
+        clearInput("#userInput");
+
+        this.questionId++;
+    };
+
+    this.checkInput = function(e, answer){
+        if (answer == self.currentSolution){
+            // Flash the ok sign,
+            // get the new question
+            // and add 3 secs in survival mode
+            flashStatus(true, "#lblStatus");
+            self.shuffle();
+            if (self.mode == "survival"){
+                self.addSeconds(3);
+            }
+        }
+        else{
+            // Flash the not ok sign,
+            // count wrong answers,
+            // and subtract 1 sec in survival mode
+            flashStatus(false, "#lblStatus");
+            self.missed++;
+            if (self.mode == "survival"){
+                self.addSeconds(-1);
+            }
+        }
+    };
+
+    this.mainTimerTick = function(){
+        // Check if there's time remaining
+        if (self.timeAvailable-- > 0){
+            // Count elapsed time and update gauge
             self.totalsecs++;
             self.gauge.refresh(self.timeAvailable);
         }
         else{
+            // Stop timer, end game
             self.stopMainTimer();
-            self.clearInput();
+            clearInput("#userInput");
             self.endGame();
         }           
     };
 
+    this.stopMainTimer = function(){
+        // Stop interval timer
+        clearInterval(self.mainTimerHandler);
+    };
+
+    this.addSeconds = function(seconds){
+        // Add or remove seconds from time remaining
+        // in survival mode.
+        var futureValue = self.timeAvailable + seconds;
+
+        // Max: 60
+        if (futureValue>60){
+            futureValue=60;
+        }
+
+        // Min: 0
+        if (futureValue<0){
+            futureValue=0;
+        }
+
+        self.timeAvailable = futureValue;
+        self.gauge.refresh(self.timeAvailable);
+        // Do some animation stuff here +3 in green, -1 in red
+    };
+
+    this.showWelcome = function() {
+        // Show welcome screen
+        hideElement("#frmResults");
+        hideElement("#frmHelp");
+        showElement("#frmWelcome");
+    };
+
     this.showInfo  = function(){
-        this.displayElement(0, "frmWelcome");
-        $('#frmWelcome').hide();
+        // Show help screen
+        hideElement("#frmWelcome");
+        showElement("#frmHelp");
+    };
 
-        $('#frmHelp').show();
-        this.displayElement(1, "frmHelp");
-    }
-
+    // Call init
     this.init();
 };
--- js/helpers.js
+++ js/helpers.js
@@ -13,3 +13,38 @@ function getSignedNumber(theNumber){
         return theNumber.toString();
     }
 }
+
+function hideElement(element){
+    fadeElement(0, element);
+    $(element).hide();
+}
+
+function showElement(element){
+    $(element).show();   
+    fadeElement(1, element);
+}
+
+function fadeElement(alpha, element){
+    $(element).animate({opacity: alpha}, 200);
+};
+
+function clearInput(inputField){
+    $(inputField).val("");
+};
+
+function setQuestion(a, b, questionField){
+    $(questionField).text(a.toString() + getSignedNumber(b) + "=");
+};
+
+function flashStatus(status, statusField){
+    if (status){
+        $(statusField).removeClass().addClass('glyphicon glyphicon-ok');
+    }
+    else{
+        $(statusField).removeClass().addClass('glyphicon glyphicon-remove');
+    }
+
+    $(statusField).animate({opacity: 1}, 200, function(){
+        $(statusField).animate({opacity: 0}, 400);
+    });
+};
